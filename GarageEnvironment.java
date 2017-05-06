c28266ee389c207d4d80db8953347dbfbea2a9fb
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2017-05-06 16:38:42 +0200

    Added perceptions for navigator.

--- GarageEnvironment.java
+++ GarageEnvironment.java
@@ -15,6 +15,7 @@ public class GarageEnvironment extends Environment {
     private String mapPath = "map.txt";
     private String carsPath = "cars.txt";
 
+    int mapx, mapy;
     private Field[][]  map;
     private List<Car> cars;
 
@@ -27,16 +28,16 @@ public class GarageEnvironment extends Environment {
             BufferedReader mapFile = new BufferedReader(new FileReader(mapPath));
 
             String[] dim = mapFile.readLine().split(" ");
-            int x = Integer.parseInt(dim[0]);
-            int y = Integer.parseInt(dim[1]);
+            mapx = Integer.parseInt(dim[0]);
+            mapy = Integer.parseInt(dim[1]);
 
-            map = new Field[x][y];    
+            map = new Field[mapx][mapy];    
 
-            for(int i=0; i<x; ++i) {
+            for(int i=0; i<mapx; ++i) {
 
                 String line = mapFile.readLine();
 
-                for(int j=0; j<y; ++j) {
+                for(int j=0; j<mapy; ++j) {
                     map[i][j] = new Field();
                     switch(line.charAt(j)) {
                         case '-': map[i][j].type = Field.Type.Road; break;
@@ -64,46 +65,53 @@ public class GarageEnvironment extends Environment {
         }
     }
 
+    private void deletePercepts() {
+        clearPercepts();
+        clearPercepts("navigator");
+        clearPercepts("surveillance");
+        clearPercepts("valet");
+    }
+
+    private void updatePercepts() {
+        try {
+            for(int i=0; i<mapx; ++i) {
+                for(int j=0; j<mapy; ++j) {
+                    String percept;
+                    switch(map[i][j].type) {
+                        case Road: percept="road"; break;
+                        case Wall: percept="wall"; break; 
+                        case ParkingSpot: percept="parkingspot"; break;
+                        case Gate: percept="gate"; break;
+                        default: percept="none"; break;
+                    }
+                    addPercept("navigator", ASSyntax.parseLiteral(""+percept+"("+i+","+j+")"));
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    } 
+
     @Override
         public void init(String[] args) {
             super.init(args);
-            try {
-                addPercept(Literal.parseLiteral("percept(demo)"));
-                addPercept("ag1", Literal.parseLiteral("p(a)"));
-                addPercept("navigator", ASSyntax.parseLiteral("n(a)"));
-                addPercept("surveillance", ASSyntax.parseLiteral("s(a)"));
-                addPercept("valet", ASSyntax.parseLiteral("v(a)"));
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            deletePercepts();
+            updatePercepts();
         }
 
     @Override
         public boolean executeAction(String agName, Structure action) {
-
             logger.info("executing: "+action+", but not implemented!");
-
             if (true) { // you may improve this condition
-
                 informAgsEnvironmentChanged();
-
             }
-
             return true; // the action was executed with success 
-
         }
 
     @Override
         public void stop() {
-            //Clear all percepts
-            clearPercepts();
-            clearPercepts("navigator");
-            clearPercepts("surveillance");
-            clearPercepts("valet");
-
+            deletePercepts();
             super.stop();
-
         }
 
 }
--- navigator.asl
+++ navigator.asl
@@ -10,3 +10,8 @@
 
 +!start : true <- .print("hello world.").
 
++road(X,Y) : true <- .print("road: ",X,",",Y).
++wall(X,Y) : true <- .print("wall: ",X,",",Y).
++parkingspot(X,Y) : true <- .print("parkingspot: ",X,",",Y).
++gate(X,Y) : true <- .print("gate: ",X,",",Y).
++none(X,Y) : true <- .print("none: ",X,",",Y).
